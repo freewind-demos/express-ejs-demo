1678700c4b18e3bde47adba22c8185ee139128a5
Peng.Li <peng.li@rea-group.com>
2016-06-23 17:57:02 +0800

    add partial template

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+// `views` is also the default value
+app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
   res.render('index', {name: 'Freewind'});
